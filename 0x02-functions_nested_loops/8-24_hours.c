d30368e72392d4c6326c054e737eff172d2097dd
B3liever <shalomjackson996@gmail.com>
2022-10-30 19:21:36 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,17 +9,17 @@ void jack_bauer(void)
 	int hour, minute;
 
 
-	for (hour = 0; hour <= 23; hr++)
+	for (hour = 0; hour <= 23; hour++)
 	{
-			for (minute = 0; minute <= 59; min++)
+			for (minute = 0; minute <= 59; minute++)
 			{
-					_putchar((hr / 10) + 48);
-					_putchar((hr % 10) + 48);
+					_putchar((hour / 10) + 48);
+					_putchar((hour % 10) + 48);
 					
 					_putchar(58);
 
-					_putchar((min / 10) + 48);
-					_putchar((min % 10) + 48);
+					_putchar((minute / 10) + 48);
+					_putchar((minute % 10) + 48);
 
 					_putchar(10);
 			}
