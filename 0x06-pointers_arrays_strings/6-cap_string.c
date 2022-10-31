4c762bcdaceade15a30042f54c05e92be941f6a7
B3liever <shalomjackson996@gmail.com>
2022-10-30 18:58:51 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,42 +1,30 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes words
- * @str: string
- * Return: pointer to string
+ * cap_string - capitalizes all words of a string
+ * @s: string
+ * Return: pointer to s
  */
-char *cap_string(char *str)
+
+char *cap_string(char *s)
 {
-	int i = 0;
+	int i, j;
 
+	char seps[] = " \t\n,;.!?\"(){}";
 
-	while (str[i])
+	for (i = 0; s[i] != '\0'; i++)
 	{
+		for (j = 0; seps[j] != '\0'; j++)
+		{
+		if (i == 0 || s[i - 1] == seps[j])
+		{
+			if (s[i] > 97 && s[i] < 123)
+				s[i] -= 32;
 
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
+		}
+		}
 
-			i++;
-
-		if (str[i - 1] == ' ' ||
-		    str[i - 1] == '\t' ||
-		    str[i - 1] == '\n' ||
-                    str[i - 1] == ','  ||
-		    str[i - 1] == ';'  ||
-		    str[i - 1] == '.'  ||
-		    str[i - 1] == '!'  ||
-		    str[i - 1] == '?'  ||
-		    str[i - 1] == '"'  ||
-		    str[i - 1] == '('  ||
-		    str[i - 1] == ')'  ||
-		    str[i - 1] == '{'  ||
-		    str[i - 1] == '}'  ||
-
-			i == 0)
-
-				str[i] -= 32;
-
-		i++;
+		return (s);
 	}
-
-	return (str);
 }
+
