e0d244d0c9054a656af64bb0b49ac59593829743
B3liever <shalomjackson996@gmail.com>
2023-01-02 14:34:20 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,11 @@
  */
 void print_numbers(void)
 {
-	int a = 0
+	int i;
 
-	while (a < 10)
-		_putchar(a++ + '0');
+	for (i = 48; i < 58; i++)
+		_putchar(i);
 
 
-	_putchar('\n');
+	_putchar(10);
 }
