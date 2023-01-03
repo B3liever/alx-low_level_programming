35c906e44ba6483a433c3b5e1e7911583f6037e4
B3liever <shalomjackson996@gmail.com>
2023-01-03 15:54:57 -0800

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - prints prime factor of 612852475143
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long i, n, n_root;
+
+	n = 612852475143;
+	n_root = sqrt(n);
+
+	for (i = 3; i < n_root; i++)
+	{
+		while ((n % i == 0) && n != i)
+			n /= i;
+	}
+
+	printf("%lu\n", n);
+
+	return (0);
+}
