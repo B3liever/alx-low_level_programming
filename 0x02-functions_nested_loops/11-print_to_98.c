cd28f924745d613d64145806ee0061d3bb1a8693
B3liever <shalomjackson996@gmail.com>
2023-01-03 14:30:10 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,30 +12,30 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-	 	for (; n < 99; n++)
+		for (; n < 99; n++)
 		{
-
 			printf("%d", n);
 
+
 			if (n < 98)
-				printf(" ,");
+				printf(", ");
+
+
 		}
 
 		printf("\n");
-
 	}
+
 	else
-	{
 		for (; n > 97; n--)
 		{
-
 			printf("%d", n);
 
 			if (n > 98)
-				printf(",");
+				printf(", ");
+
 		}
 
 		printf("\n");
-
 	}
 }
