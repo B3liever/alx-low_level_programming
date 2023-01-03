60e002c39f68ea2289bb99f95165e01493e00918
B3liever <shalomjackson996@gmail.com>
2023-01-03 01:56:59 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ * prints alphabets in lowercase
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int letter = 97;
+
+	do {
+		if (letter != 101 && letter != 113)
+		{
+			putchar(letter);
+		}
+		letter++;
+
+	} while (letter <= 122);
+
+	putchar(10);
+
+	return (0);
+}
